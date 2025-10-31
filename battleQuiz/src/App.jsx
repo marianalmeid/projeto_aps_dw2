82d480252578bd3698054951c6058b8330ce7cde
Emanuelle <emanuelle.2008@alunos.utfpr.edu.br>
2025-10-31 11:42:56 -0300

    Layout da página de cadastro

--- battleQuiz/src/App.jsx
+++ battleQuiz/src/App.jsx
@@ -1,1 +1,15 @@
 import { useState } from 'react'
+import React from "react";
+import Login from "./components/login.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
+
