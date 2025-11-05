07017b7e135454f1fc16d9a56f5bc5cf1139400a
Mariana <163425141+marianalmeid@users.noreply.github.com>
2025-11-05 11:59:54 -0300

    css do formulario

--- battleQuiz/src/components/cadastro.jsx
+++ battleQuiz/src/components/cadastro.jsx
@@ -21,21 +21,25 @@ export default function Cadastro({ irParaLogin }) {
           <div className="c-input-group">
             <i className="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" color="#673ab7" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg></i>
             <input className="cadastro-input" type="text" placeholder="NOME" />
+            <div className="space"></div>
           </div>
 
           <div className="c-input-group">
             <i className="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" color="#673ab7" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"/><rect x="2" y="4" width="20" height="16" rx="2"/></svg></i>
             <input className="cadastro-input" type="email" placeholder="E-MAIL" />
+            <div className="space"></div>
           </div>
 
           <div className="c-input-group">
             <i className="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" color="#673ab7" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-lock-keyhole-icon lucide-lock-keyhole"><circle cx="12" cy="16" r="1"/><rect x="3" y="10" width="18" height="12" rx="2"/><path d="M7 10V7a5 5 0 0 1 10 0v3"/></svg></i>
             <input className="cadastro-input" type="password" placeholder="SENHA" />
+           <div className="space"></div>
           </div>
 
           <div className="c-input-group">
             <i className="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" color="#673ab7" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-lock-icon lucide-lock"><rect width="18" height="11" x="3" y="11" rx="2" ry="2"/><path d="M7 11V7a5 5 0 0 1 10 0v4"/></svg></i>
             <input className="cadastro-input" type="password" placeholder="CONFIRMAR SENHA" />
+            <div className="space"></div>
           </div>
 
           <div className="c-input-group">
@@ -45,6 +49,7 @@ export default function Cadastro({ irParaLogin }) {
               <option value="aluno">Aluno</option>
               <option value="professor">Professor</option>
             </select>
+            <div className="space"></div>
           </div>
         </form>
         <button className="btn-cadastro">Cadastrar</button>
--- battleQuiz/src/components/login.jsx
+++ battleQuiz/src/components/login.jsx
@@ -18,6 +18,7 @@ export default function Login({ irParaCadastro }) {
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" color="#fff" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
             </i>
             <input className="login-input" type="text" placeholder="NOME OU E-MAIL" />
+            <div className="space"></div>
           </div>
 
           <div className="l-input-group">
@@ -25,6 +26,7 @@ export default function Login({ irParaCadastro }) {
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" color="#fff" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="16" r="1"/><rect x="3" y="10" width="18" height="12" rx="2"/><path d="M7 10V7a5 5 0 0 1 10 0v3"/></svg>
             </i>
             <input className="login-input" type="password" placeholder="SENHA" />
+            <div className="space"></div>
           </div>
 
           <a href="#" className="forgot-password">Esqueci minha senha</a>
