4340980f1dc6b6d87a8852e51ec52db2285a196b
Mariana <163425141+marianalmeid@users.noreply.github.com>
2025-11-14 10:00:38 -0300

    customizacao do modal

--- battleQuiz/src/components/telaInicialJog.jsx
+++ battleQuiz/src/components/telaInicialJog.jsx
@@ -4,44 +4,63 @@ import "../styles/telaInicialJog.css";
 export default function TelaInicialJogador(){
     const [abrirModal, setAbrirModal] = useState(false);
     const botaoRef = useRef(null);
+    const modalRef = useRef(null);
     const [posicao, setPosicao] = useState({top:0, left:0});
 
     //calcula para abrir o modal
     useEffect(() => {
-    if (abrirModal && botaoRef.current) {
-      const rect = botaoRef.current.getBoundingClientRect();
+        if (abrirModal && botaoRef.current && modalRef.current) {
+            const rectBotao = botaoRef.current.getBoundingClientRect();
+            const rectModal = modalRef.current.getBoundingClientRect();
       setPosicao({
-        top: rect.bottom + window.scrollY + 5, // um espacinho abaixo
-        left: rect.left + window.scrollX
-        });
-        }
+        top: rectBotao.bottom + window.scrollY + 6, // um espacinho
+        // centraliza o modal: centro do botão - metade da largura do modal
+        left:
+          rectBotao.left +
+          rectBotao.width / 2 +
+          window.scrollX -
+          rectModal.width / 2,
+      });
+    }
      }, [abrirModal]);
 
      //para fechar o modal
      useEffect(() => {
         const fechar = (e) => {
-            if (!botaoRef.current?.contains(e.target)) {
-                setAbrirModal(false);
+            if (
+              !botaoRef.current?.contains(e.target) &&
+              !modalRef.current?.contains(e.target)
+            ) {
+              setAbrirModal(false);
             }
-        };
+          };
         if (abrirModal) document.addEventListener("click", fechar);
         return () => document.removeEventListener("click", fechar);
+
+       
     }, [abrirModal]);
 
     return(
         <div className="container-telain">
             <div className="header-tij">
                 <div className="space-ti"></div>
-                <h1 className="title">Tela inicial</h1>
+                <h1 className="title">UTFPR | Battle Quiz</h1>
                 <div className="perfil-btn">
-                    <button className="icon" ref={botaoRef} onClick={() => setAbrirModal(!abrirModal)}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-user-round-icon lucide-circle-user-round" color= "var(--roxo1)"><path d="M18 20a6 6 0 0 0-12 0"/><circle cx="12" cy="10" r="4"/><circle cx="12" cy="12" r="10"/></svg></button>
+                    <button className="icon" ref={botaoRef} onClick={() => setAbrirModal(!abrirModal)}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" strokeLinecap="round"
+              strokeLinejoin="round"class="lucide lucide-circle-user-round-icon lucide-circle-user-round" color= "var(--roxo3)"><path d="M18 20a6 6 0 0 0-12 0"/><circle cx="12" cy="10" r="4"/><circle cx="12" cy="12" r="10"/></svg></button>
 
                      {/* Modal */}
                     {abrirModal && (
-                        <div className="modal-tij"
+                        <div 
+                        ref={modalRef}
+                        className="modal-tij"
                             style={{ top: posicao.top, left: posicao.left }}>
-                        <p>🧪 Modal abaixo do botão!</p>
-                        <button onClick={() => setAbrirModal(false)}>x</button>
+                            <div className="conteudo-modal">
+                                <div className="nome">nome</div>
+                                <div className="email">fulano@gmail.com</div>
+                                <div className="logout">encerrar sessão</div>
+                            </div>
+                           
                         </div>
                     )}
                 </div>
